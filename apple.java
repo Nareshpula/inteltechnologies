9fd5809dfe90bef2acee6fe2735faa14d5a2f680
NareshRoyal <NareshRoyal>
2017-06-06 03:56:08 -0700

    i commited by the apple file

---
+++ apple.java
@@ -0,0 +1,4 @@
+this is the adding the new file called apple.java
+they can easy to grasp the file.
+it is used for maintaing the job this can usefull the file.
+minimise the problem to carry out the option.
