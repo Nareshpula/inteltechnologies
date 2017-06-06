16334d55b0a8b18d7833608e9cb95e6fb2b28d71
NareshRoyal <NareshRoyal>
2017-06-06 06:59:40 -0700

    i commiting the file and i am adding something in the file

--- jani.java
+++ jani.java
@@ -1,3 +1,5 @@
 this is the very interesting to the optaining option to maintainig this file.
 they can easy to answer to this option.
 it is very interesting to the file to capture this file.
+i am adding something about this jani file.
+they can easy to ask to pay the option the logging to maintain the future.
