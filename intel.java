34db8e8af3a2276ee1a1f7494a8beac37e4d4d5c
NareshRoyal <NareshRoyal>
2017-06-06 04:03:29 -0700

    i commited to the intel project

---
+++ intel.java
@@ -0,0 +1,4 @@
+this is the file called intel and they can easy to pay the option.
+it is usefull by the intel file.
+i am looking for thhis file.i am adding something about intel project they can be participated by the 
+new things in the file.
